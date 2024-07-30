7c087413c0b6a93af46b1089ad3728e7a9c7a5bb
Emgmbakri <mrm507415@gmail.com>
2024-07-30 21:51:36 +0400

    simle falsk app with one route , 2-index

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,38 +1,44 @@
 #!/usr/bin/env python3
-"""
-Basic Flask app with Babel setup and locale selector
-"""
+'''Task 2: Get locale from request
+'''
 
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-app = Flask(__name__)
 
 class Config:
-    """
-    Configuration class for Flask-Babel
-    """
+    '''Config class'''
+
+    DEBUG = True
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
+app = Flask(__name__)
 app.config.from_object(Config)
-
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
+
 @babel.localeselector
-def get_locale():
-    """
-    Select the best match language for the user
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
+
+    Returns:
+        str: best match
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/')
-def index():
-    """
-    Route for the index page
-    """
-    return render_template('2-index.html')
+def index() -> str:
+    '''default route
+
+    Returns:
+        html: homepage
+    '''
+    return render_template("2-index.html")
 
 
 if __name__ == "__main__":
